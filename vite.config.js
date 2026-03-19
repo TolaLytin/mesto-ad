3de18ba31fa2a9de326db7888845d51194ab3f2a
TolaLytin <tolalytin@gmail.com>
2026-03-20 00:16:48 +0500

    Исправить пути для GitHub Pages test5

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  server: {
-    open: true,
-  },
   base: './',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      },
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  }
 });
